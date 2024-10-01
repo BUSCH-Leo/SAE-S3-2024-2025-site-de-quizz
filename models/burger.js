705f4e6afc21139a7a66b7ac6c245ff29ade9a29
LeoHenriot <leo.henriot58@gmail.com>
2024-10-01 10:01:17 +0200

    Ajout menu burger

---
+++ models/burger.js
@@ -0,0 +1,18 @@
+const burgerIcon = document.getElementById('burger-icon');
+const slideMenu = document.getElementById('slide-menu');
+
+let isMenuOpen = false;
+
+burgerIcon.addEventListener('click', function() {
+    slideMenu.classList.toggle('active');
+
+    if (isMenuOpen) {
+        burgerIcon.classList.remove('rotate-open');
+        burgerIcon.classList.add('rotate-close');
+    } else {
+        burgerIcon.classList.remove('rotate-close');
+        burgerIcon.classList.add('rotate-open');
+    }
+
+    isMenuOpen = !isMenuOpen;
+});
