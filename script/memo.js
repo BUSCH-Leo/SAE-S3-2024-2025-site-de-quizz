a57a82308bf94466ff465b05db6238825bc7b09b
LeoHenriot <leo.henriot58@gmail.com>
2024-10-14 09:30:18 +0200

    modification inscription et memo

--- script/memo.js
+++ script/memo.js
@@ -4,6 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
     displayJoke();
 });
 
+async function getIPAddress() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        return data.ip;
+    } catch (error) {
+        console.error('Erreur lors de la récupération de l\'adresse IP', error);
+        return 'Adresse IP non disponible';
+    }
+}
+
 // Fonction pour afficher les mémos
 function displayMemos() {
     const memoData = JSON.parse(localStorage.getItem('memos'));
@@ -43,21 +54,26 @@ function displayScore() {
 }
 
 // Fonction pour afficher des blagues
-function displayJoke() {
-    const userScore = localStorage.getItem('score') || 0; 
+async function displayJoke() {
+    const userScore = localStorage.getItem('score') || 0;
+    const userIP = await getIPAddress();
+    
     const jokes = [
-        `c'est deux fois plus que : ${Math.floor(userScore / 2)}`,
-        `c'est presque autant que : ${userScore + 1}`,
-        `vous avez dépassé le score de : ${Math.floor(Math.random() * (userScore - 1)) + 1}`,
-        `vous avez battu votre record précédent de : ${userScore - 10}`,
-        `vous êtes sur la bonne voie pour atteindre : ${userScore * 1.5}`,
-        `personne n'a fait mieux que : ${Math.floor(Math.random() * (userScore - 1)) + 1}`,
+        `C'est deux fois plus que : ${Math.floor(userScore / 2)}`,
+        `C'est presque autant que : ${Math.floor(userScore) + 1}`,
+        `Vous avez dépassé le score de : ${Math.floor(Math.random() * (userScore - 1)) + 1}`,
+        `Vous avez battu votre record précédent de : ${userScore - 10}`,
+        `Vous êtes sur la bonne voie pour atteindre : ${userScore * 1.5}`,
+        `Personne n'a fait mieux que : ${Math.floor(Math.random() * (userScore - 1)) + 1}`,
         `Vous êtes dans le top ${Math.floor(Math.random() * 99) + 1} %`,
         `Vous êtes plus intelligent que ${Math.floor(Math.random() * 99) + 1} % de la population`,
         `Vous êtes à ${Math.floor(1000 - userScore)} points de 1000 points`,
         `Vous avez aussi bien réussi le quiz que quelqu'un qui a bien réussi le quiz`,
-        `c'est quatre fois plus que : ${Math.floor(userScore / 4)}`,
-        `c'est deux fois moins que : ${Math.floor(userScore * 2)}`
+        `Vous pouvez faire mieux`,
+        `C'est quatre fois plus que : ${Math.floor(userScore / 4)}`,
+        `C'est deux fois moins que : ${Math.floor(userScore * 2)}`,
+        `User${Math.floor(Math.random() * 99999) + 10000} a obtenu ${Math.floor(Math.random() * 100) + 10} sur ce quiz`,
+        `Votre adresse IP est la suivante : ${userIP}`
     ];
 
     function getRandomJokes(jokesArray, n) {
