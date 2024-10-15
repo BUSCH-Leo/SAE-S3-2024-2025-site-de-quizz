4a5c8959d3aa0f2009a89fddcedd89a165130455
Neptune2k21 <116343729+Neptune2k21@users.noreply.github.com>
2024-10-15 18:17:14 +0200

    Update user address validation in user model

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const UserSchema = new mongoose.Schema({
     },
     address: {
         type: String,
-        required: true
+        required: false
     },
     password: {
         type: String,
