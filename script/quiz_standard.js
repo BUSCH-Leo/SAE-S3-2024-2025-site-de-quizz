a980c48ef2bc84cab7690cfdc1030585d57adc80
KORAQI <besjan.koraqii@gmail.com>
2024-11-18 14:25:37 +0100

    Création des pages et implémentation des fonction de création de quiz(Front-end))

--- script/quiz_standard.js
+++ script/quiz_standard.js
@@ -1,35 +1,184 @@
 document.addEventListener('DOMContentLoaded', function() {
     const imageUpload = document.getElementById('imageUpload');
-    const imageDisplay = document.getElementById('imageDisplay');
     const imagePreview = document.getElementById('imagePreview');
-    const timeSelect = document.getElementById('timeSelect');
-    const timeDisplay = document.getElementById('timeDisplay');
+    const quizTypeSelector = document.getElementById('quizType');
+    const questionSection = document.querySelector('.question-section');
 
-    // Prévisualisation de l'image téléchargée
-    imageUpload.addEventListener('change', function(event) {
-        const file = event.target.files[0];
-        const reader = new FileReader();
+    // Appliquer l'image de fond par défaut au chargement
+    const defaultImage = '../ressource/fond1.jpg';
+    document.body.style.backgroundImage = `url('${defaultImage}')`;
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundAttachment = 'fixed';
 
-        reader.onload = function(e) {
-            imageDisplay.style.display = 'none'; // Masquer l'image par défaut
-            imagePreview.innerHTML = `<img src="${e.target.result}" alt="Image" />`; // Afficher la prévisualisation
-        };
-
-        if (file) {
-            reader.readAsDataURL(file);
-        }
-    });
-
-    // Mise à jour du background du slider
-    function updateSliderBackground() {
-        const value = (timeSelect.value - timeSelect.min) / (timeSelect.max - timeSelect.min) * 100;
-        timeSelect.style.background = `linear-gradient(to right, #7BD634 ${value}%, #ddd ${value}%)`;
+    // Fonction pour appliquer une image en fond
+    function setBackgroundImage(imageUrl) {
+        document.body.style.backgroundImage = `url('${imageUrl}')`;
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundPosition = 'center';
+        document.body.style.backgroundAttachment = 'fixed';
     }
 
-    // Initialisation du slider
-    updateSliderBackground();
-    timeSelect.addEventListener('input', function() {
-        timeDisplay.textContent = `${timeSelect.value}s`;
-        updateSliderBackground();
-    });
+    // Gestion du slider de temps
+    function updateSliderBackground() {
+        const timeSelect = document.getElementById('timeSelect');
+        if (timeSelect) {
+            const value = (timeSelect.value - timeSelect.min) / (timeSelect.max - timeSelect.min) * 100;
+            timeSelect.style.background = `linear-gradient(to right, #7BD634 ${value}%, #ddd ${value}%)`;
+        }
+    }
+    // Fonction pour ajouter les écouteurs d'événements sur les lettres des réponses
+    function addAnswerClickListeners() {
+        const answerLetters = document.querySelectorAll('.answer-letter');
+        answerLetters.forEach(letter => {
+            letter.addEventListener('click', function() {
+                if (quizTypeSelector.value === 'vrai_ou_faux') {
+                    answerLetters.forEach(item => item.classList.remove('clicked'));
+                    letter.classList.add('clicked');
+                } else if (quizTypeSelector.value === 'multiple_choice') {
+                    letter.classList.toggle('clicked');
+                } else {
+                    answerLetters.forEach(item => item.classList.remove('clicked'));
+                    letter.classList.add('clicked');
+                }
+            });
+        });
+    }
+
+    // Initialisation du quiz type par défaut (standard) et des fonctions associées
+    function initDefaultQuiz() {
+        quizTypeSelector.value = 'standard';
+        updateQuiz();        const timeSelect = document.getElementById('timeSelect');
+        if (timeSelect) {
+            updateSliderBackground();
+        }
+        addAnswerClickListeners();
+    }
+
+    quizTypeSelector.addEventListener('change', updateQuiz);
+
+    quizTypeSelector.addEventListener('change', updateQuiz);
+
+    function updateQuiz() {
+        questionSection.innerHTML = '';
+        const mode = quizTypeSelector.value;
+
+        if (mode === 'juste_prix') {
+            questionSection.innerHTML = `
+                <input type="text" class="question-input" placeholder="Saisir une question">
+                <div align="center" class="timer">
+                    <label class="labelSelect" for="timeSelect">Choisissez le temps pour chaque question</label>
+                    <div class="slider-container">
+                        <input type="range" id="timeSelect" min="5" max="60" step="1" value="30">
+                        <span id="timeDisplay">30s</span>
+                    </div>
+                    <div class="answer-input-container">
+                        <input type="number" placeholder="Entrer le prix" class="answer-input" required>
+                        <span class="answer-letter">Prix</span>
+                    </div>
+                </div>
+                <button class="submit-btn">Valider</button>
+            `;
+        } else if (mode === 'vrai_ou_faux') {
+            questionSection.innerHTML = `
+                <input type="text" class="question-input" placeholder="Saisir une question Vrai/Faux">
+                <div align="center" class="timer">
+                    <label class="labelSelect" for="timeSelect">Choisissez le temps pour chaque question</label>
+                    <div class="slider-container">
+                        <input type="range" id="timeSelect" min="5" max="60" step="1" value="30">
+                        <span id="timeDisplay">30s</span>
+                    </div>
+                    <div class="answers">
+                        <div class="answer-input-container">
+                            <input type="text" value="Vrai" class="answer-input" required readonly>
+                            <span class="answer-letter" data-answer="A">A</span>
+                        </div>
+                        <div class="answer-input-container">
+                            <input type="text" value="Faux" class="answer-input" required readonly>
+                            <span class="answer-letter" data-answer="B">B</span>
+                        </div>
+                    </div>
+                </div>
+                <button class="submit-btn">Valider</button>
+            `;
+            addAnswerClickListeners();
+        } else if (mode === 'standard') {
+            questionSection.innerHTML = `
+                <input type="text" class="question-input" placeholder="Saisir une question avec 4 réponses possibles">
+                <div align="center" class="timer">
+                    <label class="labelSelect" for="timeSelect">Choisissez le temps pour chaque question</label>
+                    <div class="slider-container">
+                        <input type="range" id="timeSelect" min="5" max="60" step="1" value="30">
+                        <span id="timeDisplay">30s</span>
+                    </div>
+                </div>
+                <div class="answers">
+                    <div class="answer-input-container">
+                        <input type="text" placeholder="Réponse A" class="answer-input" required maxlength="24">
+                        <span class="answer-letter" data-answer="A">A</span>
+                    </div>
+                    <div class="answer-input-container">
+                        <input type="text" placeholder="Réponse B" class="answer-input" required maxlength="24">
+                        <span class="answer-letter" data-answer="B">B</span>
+                    </div>
+                    <div class="answer-input-container">
+                        <input type="text" placeholder="Réponse C" class="answer-input" required maxlength="24">
+                        <span class="answer-letter" data-answer="C">C</span>
+                    </div>
+                    <div class="answer-input-container">
+                        <input type="text" placeholder="Réponse D" class="answer-input" required maxlength="24">
+                        <span class="answer-letter" data-answer="D">D</span>
+                    </div>
+                </div>
+                <button class="submit-btn">Valider</button>
+            `;
+            addAnswerClickListeners();
+        } else if (mode === 'multiple_choice') {
+            questionSection.innerHTML = `
+                <input type="text" class="question-input" placeholder="Saisir une question avec 4 réponses possibles">
+                <div align="center" class="timer">
+                    <label class="labelSelect" for="timeSelect">Choisissez le temps pour chaque question</label>
+                    <div class="slider-container">
+                        <input type="range" id="timeSelect" min="5" max="60" step="1" value="30">
+                        <span id="timeDisplay">30s</span>
+                    </div>
+                </div>
+                <div class="answers">
+                    <div class="answer-input-container">
+                        <input type="text" placeholder="Réponse A" class="answer-input" required maxlength="24">
+                        <span class="answer-letter" data-answer="A">A</span>
+                    </div>
+                    <div class="answer-input-container">
+                        <input type="text" placeholder="Réponse B" class="answer-input" required maxlength="24">
+                        <span class="answer-letter" data-answer="B">B</span>
+                    </div>
+                    <div class="answer-input-container">
+                        <input type="text" placeholder="Réponse C" class="answer-input" required maxlength="24">
+                        <span class="answer-letter" data-answer="C">C</span>
+                    </div>
+                    <div class="answer-input-container">
+                        <input type="text" placeholder="Réponse D" class="answer-input" required maxlength="24">
+                        <span class="answer-letter" data-answer="D">D</span>
+                    </div>
+                </div>
+                <button class="submit-btn">Valider</button>
+            `;
+            addAnswerClickListeners(true);
+        }
+
+        const timeSelect = document.getElementById('timeSelect');
+        const timeDisplay = document.getElementById('timeDisplay');
+        if (timeSelect) {
+            updateSliderBackground();
+            timeDisplay.textContent = `${timeSelect.value}s`;
+
+            timeSelect.addEventListener('input', function() {
+                timeDisplay.textContent = `${timeSelect.value}s`;
+                updateSliderBackground();
+            });
+        }
+    }
+
+
+initDefaultQuiz();
 });
--- script/volet_script.js
+++ script/volet_script.js
@@ -1,64 +1,150 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const ouvrirBtn = document.querySelector('.ouvrir');
-    const fermerBtn = document.querySelector('.fermer');
-    const volet = document.querySelector('#volet');
-    const deroulerBtns = document.querySelectorAll('.derouler');
+function inviteCollaborator() {
+    const email = prompt("Entrez l'email du collaborateur :");
+    const quizId = generateQuizId();
+    const inviteUrl = window.location.href + "?quizId=" + quizId + "&invite=" + email;
+    alert("Envoyez ce lien à votre collaborateur : " + inviteUrl);
+}
 
-    // Gestion de l'ouverture du volet
-    ouvrirBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        volet.classList.add('open');
-        ouvrirBtn.style.display = 'none';
-        fermerBtn.style.display = 'block';
-    });
+    function togglePanel() {
+    const panel = document.getElementById("sidePanel");
+    const button = document.getElementById("toggleButton");
+    const buttonImage = document.getElementById("toggleButtonImage");
+    
+    panel.classList.toggle("open");
 
-    // Gestion de la fermeture du volet
-    fermerBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        volet.classList.remove('open');
-        fermerBtn.style.display = 'none';
-        ouvrirBtn.style.display = 'block';
-        
-        // Réinitialiser l'affichage des sous-menus
-        document.querySelectorAll('.sous-quiz, .sous-option, .sous-theme').forEach(subMenu => {
-            subMenu.style.display = 'none';
-            const parentDiv = subMenu.previousElementSibling;
-            parentDiv.classList.remove('open');
-        });
-    });
+    if (panel.classList.contains("open")) {
+        button.style.right = "350px";
+        buttonImage.src = "../ressource/page_creation_quiz/fermer.png";
+    } else {
+        button.style.right = "0px";
+        buttonImage.src = "../ressource/page_creation_quiz/ouvrir.png";
+    }
+}
 
-    // Gestion de l'affichage des sous-menus (Quiz, Option, Thème)
-    deroulerBtns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            const sousMenu = this.parentElement.nextElementSibling; 
-            const parentDiv = this.parentElement;
+    function showTab(tab) {
+        const colorTab = document.getElementById("colorTab");
+        const imageTab = document.getElementById("imageTab");
+        const tabs = document.querySelectorAll(".tab");
 
-            // Toggle de l'affichage du sous-menu
-            if (sousMenu.style.display === 'block') {
-                sousMenu.style.display = 'none';
-                parentDiv.classList.remove('open'); 
-                this.classList.remove('rotated'); 
-            } else {
-                sousMenu.style.display = 'block';
-                parentDiv.classList.add('open'); 
-                this.classList.add('rotated'); 
-            }
-        });
-    });
-
-    // Fermer le volet en cliquant à l'extérieur
-    document.addEventListener('click', function(event) {
-        if (!volet.contains(event.target) && !ouvrirBtn.contains(event.target)) {
-            if (volet.classList.contains('open')) {
-                fermerBtn.click(); // Simule le clic sur le bouton fermer
-            }
+        if (tab === 'colors') {
+            colorTab.style.display = 'block';
+            imageTab.style.display = 'none';
+            tabs[0].classList.add('active');
+            tabs[1].classList.remove('active');
+        } else {
+            colorTab.style.display = 'none';
+            imageTab.style.display = 'block';
+            tabs[0].classList.remove('active');
+            tabs[1].classList.add('active');
         }
-    });
-});
+    }
 
-// Fonction pour changer le thème
-function changeTheme(theme) {
-    const volet = document.getElementById('volet');
-    volet.classList.remove('theme1', 'theme2', 'theme3'); // Réinitialiser les thèmes
-    volet.classList.add(theme); // Ajouter le thème sélectionné
+    function updateTheme(isDarkMode) {
+      const body = document.body;
+      const panel = document.getElementById("sidePanel");
+
+      if (isDarkMode) {
+        body.style.setProperty("--background-color", "hsl(0deg 0.18% 32.37%)");
+        body.style.setProperty("--text-color", "hsl(228, 5%, 80%)");
+        panel.style.setProperty("--background-color", "hsl(0deg 0% 37.16%)");
+        panel.style.setProperty("--text-color", "hsl(228, 5%, 80%)");
+      } else {
+        body.style.setProperty("--background-color", "#FFF");
+        body.style.setProperty("--text-color", "#222");
+        panel.style.setProperty("--background-color", "#FFF");
+        panel.style.setProperty("--text-color", "#222");
+      }
+    }
+
+    function setBackgroundColor(color) {
+        document.body.style.backgroundImage = "none";
+        document.body.style.backgroundColor = color;
+    }
+
+    function setBackgroundImage(imageUrl) {
+        document.body.style.backgroundImage = `url(${imageUrl})`;
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+        document.body.style.backgroundRepeat = "no-repeat";
+    }
+
+    function saveQuiz() {
+        const quizType = document.getElementById("quizType").value;
+
+        localStorage.setItem("savedQuiz", quizType);
+        alert("Le quiz a été enregistré !");
+        
+        console.log("Quiz enregistré : Type de quiz -", quizType);
+    }
+
+    function shareQuiz() {
+    const quizLink = "file:///C:/Users/bk030481/Desktop/Nouveau%20dossier/test.html"; 
+
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(quizLink)}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(quizLink)}&text=Découvrez ce quiz incroyable !`;
+    const linkedinUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(quizLink)}`;
+    const instaUrl = `https://www.instagram.com/?url=${encodeURIComponent(quizLink)}`;
+
+    const socialChoice = prompt("Sur quel réseau souhaitez-vous partager ? (facebook, twitter, linkedin, instagram)");
+
+    let shareUrl;
+    switch (socialChoice.toLowerCase()) {
+        case 'facebook':
+            shareUrl = facebookUrl;
+            break;
+        case 'twitter':
+            shareUrl = twitterUrl;
+            break;
+        case 'linkedin':
+            shareUrl = linkedinUrl;
+            break;
+        case 'instagram':
+            shareUrl = instaUrl;
+            break;
+        default:
+            alert("Veuillez choisir entre 'facebook', 'twitter', 'linkedin' ou 'Instagram.");
+            return;
+    }
+    window.open(shareUrl, '_blank');
+    alert("Le quiz a été partagé !");
+}
+const translations = {
+  en: {
+    quizType: "Quiz Type",
+    h3: "Choose Theme (Background Color)",
+    h2: "Quiz Settings",
+    chooseQuizType: "Choose a type",
+    colorTab: "Colors",
+    imageTab: "Images",
+    backgroundColor: "Choose Background Color",
+    backgroundImage: "Choose Background Image",
+    submit: "Save Quiz",
+    share: "Share Quiz"
+  },
+  fr: {
+    quizType: "Type de Quiz",
+    h3: "Choisissez le Thème (Couleur de Fond)",
+    h2: "Paramètres du Quiz",
+    chooseQuizType: "Choisissez un type",
+    colorTab: "Couleurs",
+    imageTab: "Images",
+    backgroundColor: "Choisissez la couleur de fond",
+    backgroundImage: "Choisissez une image de fond",
+    submit: "Enregistrer le Quiz",
+    share: "Partager le Quiz"
+  }
+};
+
+function changeLanguage() {
+  const selectedLanguage = document.getElementById('languageSelector').value;
+  const translation = translations[selectedLanguage];
+
+  document.querySelector('label[for="quizType"]').textContent = translation.quizType;
+  document.getElementById('quizType').setAttribute('placeholder', translation.chooseQuizType);
+  document.querySelector('.tab:nth-child(1)').textContent = translation.colorTab;
+  document.querySelector('.tab:nth-child(2)').textContent = translation.imageTab;
+  document.querySelector('.theme-section button.submit-button').textContent = translation.submit;
+  document.querySelector('.theme-section button.submit-button + button').textContent = translation.share;
+   document.querySelector('h3').textContent = translation.h3;
+   document.querySelector('h2').textContent = translation.h2;
 }
